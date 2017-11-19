2e0ab8645429df95094bd43ed8d7b682df65c8cb
gittyanand <33614594+gittyanand@users.noreply.github.com>
2017-11-19 08:26:59 -0600

    TemplateDrivenForms(TDF)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles:[
+    `input.ng-invalid{ border-left:5px solid red }
+    input.ng-valid{ border-left:5px solid green }
+    `
+  ]
 })
 export class AppComponent {
   title = 'app';
+  myName="OmSriSaiRam";
+
+  OnSubmit(value:any)
+  {
+    console.log(value);
+  }
 }
